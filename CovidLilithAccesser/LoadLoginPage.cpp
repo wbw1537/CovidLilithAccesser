f038be141c7996183a2f2bdac8077d8738ad9669
X14oGu4n <2712299561@qq.com>
2022-06-29 16:07:21 +0800

    登录选项

--- CovidLilithAccesser/LoadLoginPage.cpp
+++ CovidLilithAccesser/LoadLoginPage.cpp
@@ -10,7 +10,17 @@ void LoadLoginPage() {
 	loadimage(&logo, "resources/covidProofWorkers.jpg",350,227);
 	putimage(0, 0, &backGround);
 	putimage(80, WINDOWHEIGHT / 2 - logo.getheight() / 2 - 20, &logo);
-	fillrectangle(270, 445, 370, 490);
-	fillrectangle(390, 445, 490, 490);
-	fillrectangle(510, 445, 610, 490);
+	fillrectangle(250, 445, 400, 490);
+	fillrectangle(420, 445, 570, 490);
+	fillrectangle(590, 445, 740, 490);
+	setbkmode(TRANSPARENT);
+	settextcolor(GREEN);
+	char FirstSelect1[20] = { "管理员登录" };
+	char FirstSelect2[20] = { "社区工作者登录" };
+	char FirstSelect3[20] = { "居民登录" };
+	RECT R[3] = { {250, 445, 400, 490},{420, 445, 570, 490},{590, 445, 740, 490} };
+	drawtext(FirstSelect1, &R[0], DT_CENTER | DT_VCENTER | DT_SINGLELINE);
+	drawtext(FirstSelect2, &R[1], DT_CENTER | DT_VCENTER | DT_SINGLELINE);
+	drawtext(FirstSelect3, &R[2], DT_CENTER | DT_VCENTER | DT_SINGLELINE);
+
 }
