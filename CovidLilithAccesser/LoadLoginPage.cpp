7b36eb7f8cf4a8460e4f8b587dd7e71e7bd0ae15
X14oGu4n <2712299561@qq.com>
2022-06-29 15:35:00 +0800

    new repo

--- CovidLilithAccesser/LoadLoginPage.cpp
+++ CovidLilithAccesser/LoadLoginPage.cpp
@@ -1,11 +1,15 @@
 #include"CovidLilithAccesser.h"
+#include"LoadLoginPage.h"
 
 //load the login page
 void LoadLoginPage() {
+	
 	IMAGE backGround, logo;
 	//load background image and logo image, then print them.
-	loadimage(&backGround, "resources / backGround.png", 960, 540);
-	loadimage(&logo, "resources/covidProofWorkers.jpg");
+	loadimage(&backGround, "resources/backGround.jpg");
+	loadimage(&logo, "resources/covidProofWorkers.jpg",350,227);
 	putimage(0, 0, &backGround);
-	putimage(0, 0, &logo);
+	putimage(80, WINDOWHEIGHT / 2 - logo.getheight() / 2 - 20, &logo);
+
+	
 }
