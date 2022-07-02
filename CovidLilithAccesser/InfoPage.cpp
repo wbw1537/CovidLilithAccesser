30d8a6fd31126d3afcf73739dc9daa06a8cffb0d
wbw1537 <wbw1537@qq.com>
2022-07-02 20:45:21 +0800

    texts

--- CovidLilithAccesser/InfoPage.cpp
+++ CovidLilithAccesser/InfoPage.cpp
@@ -15,9 +15,14 @@ void ReciManageMenu(){
 	drawAlpha(&title, 630, 30);
 	do {
 		m3 = GetMouseMsg();
-		for (int i = 0; i < numOfRes; i++){
-			DrawLineButtonOfRes(10, 10, 80, 40, 20, &residentInfo[i], m3);
-		}
+		DrawLineButtonOfRes(10, 10, 80, 40, 20, &residentInfo[1], m3);
+		DrawLineButtonOfRes(10, 50, 80, 40, 20, &residentInfo[2], m3);
+		DrawLineButtonOfRes(10, 90, 80, 40, 20, &residentInfo[3], m3);
+		DrawLineButtonOfRes(10, 130, 80, 40, 20, &residentInfo[4], m3);
+		DrawLineButtonOfRes(10, 170, 80, 40, 20, &residentInfo[5], m3);
+		DrawLineButtonOfRes(10, 210, 80, 40, 20, &residentInfo[6], m3);
+		DrawLineButtonOfRes(10, 250, 80, 40, 20, &residentInfo[7], m3);
+
 		
 	} while (1);
 }
